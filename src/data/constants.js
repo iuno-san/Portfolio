bd9165d1f7bb6d0f4a85283dfa8927c9c6e3302d
Ignacy Starczynski <ignacysan27@gmail.com>
2024-07-23 12:09:00 +0200

    Update data

--- src/data/constants.js
+++ src/data/constants.js
@@ -132,7 +132,7 @@ export const experiences = [
   },
   {
     id: 2,
-    img: "https://raw.githubusercontent.com/Chillcode-Official/ChillCode/main/img/Icons/icon_c-sharp.png",
+    img: "https://raw.githubusercontent.com/iuno-san/ChillCode/main/chillcode/img/Icons/icon_c-sharp.png",
     role: "Programming Tutor",
     company: "FreeLancer",
     date: "November 2022 - June 2023",
@@ -156,17 +156,17 @@ export const projects = [
     description:
       "ChillCode is a platform for learning programming with an extensive database of tutorials ",
     image:
-      "https://raw.githubusercontent.com/Chillcode-Official/ChillCode/main/img/Others/blog-img(1).png",
+      "https://raw.githubusercontent.com/iuno-san/ChillCode/main/chillcode/img/Others/blog-img(1).png",
     tags: ["Website", "Coding", "Tutorials", "Platform", "HTML"],
     category: "live app",
     github: "https://github.com/Chillcode-Official",
-    webapp: "https://www.chillcode.org/",
+    webapp: "https://chillcode.vercel.app",
     member: [
       {
         name: "Ignacy Starczynski",
         img: "https://avatars.githubusercontent.com/u/117463347?s=64&v=4",
         linkedin: "https://www.linkedin.com/in/ignacy-starczynski-8030b2284/",
-        github: "https://github.com/iuno-san",
+        github: "https://github.com/iuno-san/ChillCode",
       },
     ],
   },
